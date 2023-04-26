db79e0dddb02fc977155143092fd2ebe5203ac0c
Pokego01 <129116026+Pokego01@users.noreply.github.com>
2023-04-26 14:23:08 -0400

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,21 @@
+##Simple Print
+print("Mi Nombre es Daniel")
+
+##DECLARAR VARIABLES##
+NOMBRE="Daniel"
+NAME="Jose"
+print ("Mi Nombre es", NAME)
+
+##Declarar edad
+Edad=20
+
+##Impresion
+print("mi edad es", Edad)
+
+##Imprimir ahora ambas variables
+print("Hi, Mi name is", NAME, "I'm", Edad,"Years old") 
+
+Nombre1=input("cual es tu nombre?"/n)
+Edad1=insput("Cual es tu edad?"/n)
+
+print("Tu nombre es:", Nombre1, "y tu edad es", Edad1,"años")
