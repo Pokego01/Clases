d51c7fadd869778ff8b3ec40904de8d39d6f57d6
Pokego01 <129116026+Pokego01@users.noreply.github.com>
2023-06-23 15:29:14 -0400

    Add files via upload

--- ejercicio.py
+++ ejercicio.py
@@ -1,12 +1,3 @@
-nombre = input("Cual es el nombre del alumno?\n")
-asignatura = input("Cual es la asignatura en la que se evaluo?\n")
-lab1 = float(input("Cual fue la nota del laboratorio 1?\n"))
-lab2 = float(input("Cual fue la nota del laboratorio 2?\n"))
-promedio = float(lab1*0.3 + lab2*0.7)
-x = {
-    "Nombre":nombre,
-    "Asignatura":asignatura,
-    "Nota Lab,1":lab1,
-    "Nota Lab.2":lab2,
-    "Promedio":f"{promedio:.1f}"}
-print(x)
+nombre = input("CUAL ES TU NOMBRE?\n")
+edad = int(input("CUAL ES TU EDAD?\n"))
+print("Mi nombre es", nombre, "mi edad es", edad, "años y en 20 años tendre", edad+20, "años")
