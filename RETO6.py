cb299a0e85b9e774b3d98f3f3e7dc981bb3715e6
Pokego01 <129116026+Pokego01@users.noreply.github.com>
2023-06-23 15:36:42 -0400

    Add files via upload

---
+++ RETO6.py
@@ -0,0 +1,6 @@
+num=list(range(1,31))
+for x in num[9:30]:
+    if x%2==0:
+        print(x)
+        y=x + y
+        print(y)
---
+++ reto5.py
@@ -0,0 +1,13 @@
+x=int(input("ingrese un numero\n"))
+
+if x%2==0:
+    print("su numero es par")
+else:
+    print("su nuemro es impar")
+
+if x>50:
+    print("sumero es mayor a 50")
+elif x==50:
+    print("su numero es 50")
+else:
+    print("su nuemro es menor a 50")
